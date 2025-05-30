7eb885063f10c86a92d02931decee2002d8c6d98
Yevhen Onoprienko <fromzpcity@gmail.com>
2025-05-31 00:41:43 +0300

    03. usefull-things-180-slider. THREE animation.

---
+++ src/assets/js/slides.js
@@ -0,0 +1,37 @@
+export const slides = [
+    {
+        title: "Moons's End",
+        url: "https://www.google.com",
+        image: "./src/assets/images/img1.jpg",
+    },
+    {
+        title: "Broken Thorns",
+        url: "https://www.google.com",
+        image: "./src/assets/images/img2.jpg",
+    },
+    {
+        title: "Stars in the Illusion",
+        url: "https://www.google.com",
+        image: "./src/assets/images/img3.jpg",
+    },
+    {
+        title: "Cracked Shard",
+        url: "https://www.google.com",
+        image: "./src/assets/images/img4.jpg",
+    },
+    {
+        title: "Ring in the Abyss",
+        url: "https://www.google.com",
+        image: "./src/assets/images/img5.jpg",
+    },
+    {
+        title: "Nothing of Dragon",
+        url: "https://www.google.com",
+        image: "./src/assets/images/img6.jpg",
+    },
+    {
+        title: "Tales of Dreamer",
+        url: "https://www.google.com",
+        image: "./src/assets/images/img7.jpg",
+    },
+];
